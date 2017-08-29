195f85fd34410dbb854d62f9feb1206705c32361
Sandro Mani <manisandro@gmail.com>
2017-08-29 18:37:13 +0200

    Add german translation

---
+++ lib/locale/GdbCrashHandler_de.ts
@@ -0,0 +1,94 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>GdbCrashHandlerDialog</name>
+    <message>
+        <location filename="../GdbCrashHandlerDialog.ui" line="20"/>
+        <source>Error Report</source>
+        <translation>Fehlerbericht</translation>
+    </message>
+    <message>
+        <location filename="../GdbCrashHandlerDialog.ui" line="84"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:16pt;&quot;&gt;%1 has stopped working&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:16pt;&quot;&gt;%1 funktioniert nicht mehr&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../GdbCrashHandlerDialog.ui" line="150"/>
+        <source>Plase send us this report to help fix the problem and improve this software.</source>
+        <translation>Bitte schicken Sie diesen Bericht, sodass der Fehler korrigiert werden kann.</translation>
+    </message>
+    <message>
+        <location filename="../GdbCrashHandlerDialog.ui" line="176"/>
+        <source>Show report contents</source>
+        <translation>Berichtsinhalt anzeigen</translation>
+    </message>
+    <message>
+        <location filename="../GdbCrashHandlerDialog.ui" line="223"/>
+        <source>Regenerate stacktrace</source>
+        <translation>Stracktrace neugenerieren</translation>
+    </message>
+    <message>
+        <location filename="../GdbCrashHandlerDialog.ui" line="243"/>
+        <source>Provide additional info about the problem (recommended)</source>
+        <translation>Mehr Informationen zum Problem angeben (empfohlen)</translation>
+    </message>
+    <message>
+        <location filename="../GdbCrashHandlerDialog.ui" line="256"/>
+        <source>What were you doing when the crash occurred?</source>
+        <translation>Was waren Sie am machen, als diesen Fehler auftrat?</translation>
+    </message>
+    <message>
+        <location filename="../GdbCrashHandlerDialog.ui" line="266"/>
+        <source>Contact for additional information (optional):</source>
+        <translation>Kontakt für mehr Informationen (optional):</translation>
+    </message>
+    <message>
+        <location filename="../GdbCrashHandlerDialog.ui" line="279"/>
+        <source>Restart the application</source>
+        <translation>Anwendung neustarten</translation>
+    </message>
+    <message>
+        <location filename="../GdbCrashHandlerDialog.cpp" line="44"/>
+        <source>&lt;b&gt;Your work was saved to &lt;i&gt;%1&lt;/i&gt;.&lt;/b&gt;</source>
+        <translation>&lt;b&gt;Ihre Arbeit wurde in &lt;i&gt;%1&lt;/i&gt; gespeichert.&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="../GdbCrashHandlerDialog.cpp" line="58"/>
+        <source>Send report</source>
+        <translation>Bericht schicken</translation>
+    </message>
+    <message>
+        <location filename="../GdbCrashHandlerDialog.cpp" line="60"/>
+        <source>Don&apos;t send report</source>
+        <translation>Bericht nicht schicken</translation>
+    </message>
+    <message>
+        <location filename="../GdbCrashHandlerDialog.cpp" line="86"/>
+        <source>Failed to obtain backtrace. Is gdb installed?</source>
+        <translation>Stacktrace konnte nicht erzeugt werden. Ist gdb installiert?</translation>
+    </message>
+    <message>
+        <location filename="../GdbCrashHandlerDialog.cpp" line="179"/>
+        <location filename="../GdbCrashHandlerDialog.cpp" line="181"/>
+        <location filename="../GdbCrashHandlerDialog.cpp" line="188"/>
+        <source>Submit failed</source>
+        <translation>Sendefehler</translation>
+    </message>
+    <message>
+        <location filename="../GdbCrashHandlerDialog.cpp" line="179"/>
+        <source>The attempt to submit the report timed out.</source>
+        <translation>Timeout beim Senden des Berichts.</translation>
+    </message>
+    <message>
+        <location filename="../GdbCrashHandlerDialog.cpp" line="181"/>
+        <source>The attempt to submit the report failed: %1</source>
+        <translation>Fehler beim Senden des Breichts: %1</translation>
+    </message>
+    <message>
+        <location filename="../GdbCrashHandlerDialog.cpp" line="188"/>
+        <source>No submit method was configured by the application.</source>
+        <translation type="unfinished">Kein Sendedienst wurde von der Applikation konfiguriert.</translation>
+    </message>
+</context>
+</TS>
---
+++ lib/locale/locale.pro
@@ -0,0 +1,5 @@
+SOURCES += ../*.cpp
+FORMS += ../*.ui
+
+TRANSLATIONS += \
+  GdbCrashHandler_de.ts
